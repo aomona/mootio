7d1b8fb4caccf03ce70600350d5541edb916606c
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2026-02-04 21:46:50 +0900

    ホームページ

--- app/(page)/page.tsx
+++ app/(page)/page.tsx
@@ -1,3 +1,21 @@
+import Image from "next/image";
 export default function Home() {
-	return <div className="space-y-10"></div>;
+	return (
+		<div className="bg-black w-screen h-screen">
+			<div className="flex items-center w-full h-full justify-center">
+				<div className="flex items-center flex-col">
+					<Image
+						src="/images/shake/f2d5e329-0736-463e-817a-3b3269b6acc2.svg"
+						alt="Logo"
+						width={100}
+						height={100}
+						className="mb-10"
+					/>
+					<p className="text-white text-2xl ml-4 text-center">
+						このページをホーム画面に追加、もしくはインストールを行なって開いてください。
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
